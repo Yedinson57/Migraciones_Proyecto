d562523cffae524664c278a700bec33f8995d481
Yedinson57 <yedinson960@gmail.com>
2026-04-24 11:42:19 -0500

    migraciones_proyecto

---
+++ database/migrations/2026_04_24_161340_create_teachers_table.php
@@ -0,0 +1,48 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     */
+    public function up(): void
+    {
+        Schema::create('teachers', function (Blueprint $table) {
+            $table->id();
+            $table->String('name');
+            $table->String('email');
+
+            //Llave foranea area
+            $table->unsignedBigInteger('area_id')->nullable()->unique();
+
+            $table->foreign('area_id')
+                ->references('id')
+                ->on('areas')
+                ->onDelete('set null')
+                ->onUpdate('set null');
+
+            //Llave foranea training_center
+            $table->unsignedBigInteger('training_center_id')->nullable()->unique();
+
+            $table->foreign('training_center_id')
+                ->references('id')
+                ->on('training_centers')
+                ->onDelete('set null')
+                ->onUpdate('set null');
+                
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     */
+    public function down(): void
+    {
+        Schema::dropIfExists('teachers');
+    }
+};
